81e8fce16171c52dad1e3b7af8729e07778f0a17
Sanchit Rk <sanchitrrk@gmail.com>
2026-04-28 18:57:33 +0530

    wip

--- packages/sdk/src/client.ts
+++ packages/sdk/src/client.ts
@@ -1,37 +1,29 @@
-import {CodeWorkSdk} from "./generated";
-import {type Config} from "./generated/client"
-import {createClient} from "./generated/client";
+import { CodeWorkSdk } from "./generated";
+import { createClient, type Config } from "./generated/client";
 
 export { type Config as CodeWorkClientConfig, CodeWorkSdk as CodeWorkSdkClient };
 
-export function createCodeWorkClient(config?: Config & { directory?: string; workspaceId?: string }) {
-	if (!config?.fetch) {
-		const customFetch: any = (req: any) => {
-			req.timeout = false
-			return fetch(req)
-		}
-		config = {
-			...config,
-			fetch: customFetch,
-		}
+export function createCodeWorkClient(config: Config & { directory?: string; workspaceId?: string } = {}) {
+	const { directory, workspaceId, ...clientConfig } = config;
+	const headers = new Headers(clientConfig.headers as HeadersInit);
+
+	if (directory) {
+		headers.set("x-codework-directory", encodeURIComponent(directory));
 	}
 
-	if (config?.directory) {
-		const isNonASCII = /[^\x00-\x7F]/.test(config.directory)
-		const encodedDirectory = isNonASCII ? encodeURIComponent(config.directory) : config.directory
-		config.headers = {
-			...config.headers,
-			"x-codework-directory": encodedDirectory,
-		}
+	if (workspaceId) {
+		headers.set("x-codework-workspace", workspaceId);
 	}
 
-	if (config?.workspaceId) {
-		config.headers = {
-			...config.headers,
-			"x-codework-workspace": config.workspaceId,
-		}
-	}
+	const customFetch: typeof fetch = (request, init) => {
+		if (request instanceof Request) (request as Request & { timeout?: boolean }).timeout = false;
+		return fetch(request, init);
+	};
 
-	const client = createClient(config)
-	return new CodeWorkSdk({ client })
+	const client = createClient({
+		...clientConfig,
+		fetch: clientConfig.fetch ?? customFetch,
+		headers,
+	});
+	return new CodeWorkSdk({ client });
 }
