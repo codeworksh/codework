b3f6b1b94bf08cfee48f08bd94b425b8795142d6
Sanchit Rk <sanchitrrk@gmail.com>
2026-06-09 15:57:47 +0530

    wip: VFS

--- packages/harness/src/filesystem/filesystem.ts
+++ packages/harness/src/filesystem/filesystem.ts
@@ -1,4 +1,5 @@
-import { Effect, Schema, Context, Layer } from "effect";
+import { create, RealFSProvider, type VirtualFileSystem } from "@platformatic/vfs";
+import { Context, Effect, Layer, Schema } from "effect";
 
 export class FileSystemError extends Schema.TaggedErrorClass<FileSystemError>()("FileSystemError", {
 	method: Schema.String,
@@ -12,4 +13,36 @@ export interface Interface {
 
 export class Service extends Context.Service<Service, Interface>()("@codework/filesystem") {}
 
-export const layer = Layer.effect(Service);
+export class Vfs extends Context.Service<Vfs, VirtualFileSystem>()("@codework/filesystem/Vfs") {}
+
+const attempt = <A>(method: string, evaluate: () => Promise<A>) =>
+	Effect.tryPromise({
+		try: evaluate,
+		catch: (cause) => new FileSystemError({ method, cause }),
+	});
+
+export const layer = Layer.effect(
+	Service,
+	Effect.gen(function* () {
+		const vfs = yield* Vfs;
+
+		return Service.of({
+			readFileString: (path, encoding = "utf8") =>
+				attempt("readFileString", () => vfs.promises.readFile(path, encoding as BufferEncoding)),
+			writeFileString: (path, data) => attempt("writeFileString", () => vfs.promises.writeFile(path, data)),
+		});
+	}),
+);
+
+export const layerFromVfs = (vfs: VirtualFileSystem) => Layer.succeed(Vfs, vfs);
+
+export const NodeFileSystem = {
+	layer: (rootPath: string) =>
+		layerFromVfs(
+			create(new RealFSProvider(rootPath), {
+				moduleHooks: false,
+			}),
+		),
+} as const;
+
+export const defaultLayer = (rootPath: string) => layer.pipe(Layer.provide(NodeFileSystem.layer(rootPath)));
